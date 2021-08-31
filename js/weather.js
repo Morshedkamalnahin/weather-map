5e4aed00c987d84a7e27f9dbf87638dfda94665f
Morshed Kamal Nahin <86628012+Morshedkamalnahin@users.noreply.github.com>
2021-09-01 02:38:42 +0600

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -29,7 +29,7 @@ const tempareture = data => {
     } else {
 
         const div = document.createElement('div');
-        div.innerHTML = `  <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="">
+        div.innerHTML = `  <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}" alt="">
             <h1 id="city-name">${data.name}</h1>
             <h3><span id="tempa">${data.main.temp}</span>&deg;C</h3>
             <h1 id="weather-status" class="lead">${data.weather[0].main}</h1>`
